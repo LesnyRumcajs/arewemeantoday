bc3631aa58119d5c7e56893b5c048ca717186d25
lesnyrumcajs <lesny.rumcajs@gmail.com>
2020-03-23 21:30:19 +0100

    working status

--- src/main.rs
+++ src/main.rs
@@ -2,12 +2,36 @@
 
 #[macro_use] extern crate rocket;
 
-#[get("/hello/<name>/<age>")]
-fn hello(name: String, age: u8) -> String {
-    format!("Hello, {} year old named {}!", age, name)
+use chrono::{Datelike, Utc};
+
+
+use rocket::response::NamedFile;
+use rocket_contrib::serve::StaticFiles;
+
+use digest::Digest;
+use sha2::Sha256;
+use digest::generic_array::functional::FunctionalSequence;
+
+fn mean_check() -> bool {
+    let mut hasher = Sha256::new();
+    let now = Utc::now();
+    hasher.input(format!("{}{}{}", now.year(), now.month(), now.day()).as_bytes());
+    hasher.result().fold(0, |acc, x| acc + x.count_ones()) % 2 == 0
+}
+
+#[get("/")]
+fn are_we_mean_today() -> NamedFile {
+    if mean_check() {
+        NamedFile::open("static/mean.html").unwrap()
+    } else {
+        NamedFile::open("static/not_mean.html").unwrap()
+    }
 }
 
 fn main() {
-    rocket::ignite().mount("/", routes![hello]).launch();
+    rocket::ignite()
+        .mount("/public", StaticFiles::from("static"))
+        .mount("/", routes![are_we_mean_today])
+        .launch();
 }
 
