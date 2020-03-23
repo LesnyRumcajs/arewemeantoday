414f83c7225ae4740df10613aa3bbd2b0e307c15
lesnyrumcajs <lesny.rumcajs@gmail.com>
2020-03-23 22:06:15 +0100

    cleanup

---
+++ README.md
@@ -0,0 +1,22 @@
+# Are we mean today
+Daily grumpiness compass. 
+
+
+---
+### Buidling & deploying Rocket-Heroku application
+Ensure you have `heroku-cli` installed on your machine.
+```
+heroku create --buildpack emk/rust arewemeantoday
+echo 'web: ROCKET_PORT=$PORT ROCKET_ENV=prod ./target/release/arewemeantoday' > Procfile
+# Rocket requires nightly Rust at the moment.
+echo 'VERSION=nightly' > RustConfig
+
+... # do the development, commit what you have
+
+# if first run
+git push --set-upstream heroku master
+# consecutive ones
+git push heroku master
+```
+
+That's really all! Enjoy! 🦀🚀
--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,7 @@
 #![feature(proc_macro_hygiene, decl_macro)]
-
 #[macro_use] extern crate rocket;
 
 use chrono::{Datelike, Utc};
-
 
 use rocket::response::NamedFile;
 use rocket_contrib::serve::StaticFiles;
@@ -11,6 +9,8 @@ use rocket_contrib::serve::StaticFiles;
 use digest::Digest;
 use sha2::Sha256;
 use digest::generic_array::functional::FunctionalSequence;
+
+static RESOURCE_DIRECTORY: &str = "static";
 
 fn mean_check() -> bool {
     let mut hasher = Sha256::new();
@@ -22,16 +22,15 @@ fn mean_check() -> bool {
 #[get("/")]
 fn are_we_mean_today() -> NamedFile {
     if mean_check() {
-        NamedFile::open("static/mean.html").unwrap()
+        NamedFile::open(format!("{}/{}", RESOURCE_DIRECTORY, "mean.html"))
     } else {
-        NamedFile::open("static/not_mean.html").unwrap()
-    }
+        NamedFile::open(format!("{}/{}", RESOURCE_DIRECTORY, "not_mean.html"))
+    }.expect("Invalid file!")
 }
 
 fn main() {
     rocket::ignite()
-        .mount("/public", StaticFiles::from("static"))
+        .mount("/public", StaticFiles::from(RESOURCE_DIRECTORY))
         .mount("/", routes![are_we_mean_today])
         .launch();
 }
-
