8c90c8c9a46f6d55e1f338c7e241f74983ecdcd2
lesnyrumcajs <lesny.rumcajs@gmail.com>
2020-04-02 23:23:31 +0200

    add test if we are not too mean (but still mean)

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,7 @@
 #[macro_use]
 extern crate rocket;
 
-use chrono::{Datelike, Utc};
+use chrono::{DateTime, Datelike, Utc};
 
 use rocket::response::NamedFile;
 use rocket_contrib::serve::StaticFiles;
@@ -14,16 +14,15 @@ use sha2::Sha256;
 static RESOURCE_DIRECTORY: &str = "static";
 static SALT: &str = "f07Y4QY8Zwk1eUmQC6ZxayfT";
 
-fn mean_check() -> bool {
+fn mean_check(date: DateTime<Utc>) -> bool {
     let mut hasher = Sha256::new();
-    let now = Utc::now();
-    hasher.input(format!("{}{}{}{}", SALT, now.year(), now.month(), now.day()).as_bytes());
+    hasher.input(format!("{}{}{}{}", SALT, date.year(), date.month(), date.day()).as_bytes());
     hasher.result().fold(0, |acc, x| acc + x.count_ones()) % 2 == 0
 }
 
 #[get("/")]
 fn are_we_mean_today() -> NamedFile {
-    if mean_check() {
+    if mean_check(Utc::now()) {
         NamedFile::open(format!("{}/{}", RESOURCE_DIRECTORY, "mean.html"))
     } else {
         NamedFile::open(format!("{}/{}", RESOURCE_DIRECTORY, "not_mean.html"))
@@ -36,4 +35,19 @@ fn main() {
         .mount("/public", StaticFiles::from(RESOURCE_DIRECTORY))
         .mount("/", routes![are_we_mean_today])
         .launch();
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use chrono::Duration;
+
+    #[test]
+    fn check_we_are_not_too_mean_but_still_mean() {
+        let date = Utc::now();
+        let mean_days = (0..10000i64)
+            .filter(|x| mean_check(date + Duration::days(*x)))
+            .count();
+        assert!(mean_days > 4000 && mean_days < 6000)
+    }
 }
